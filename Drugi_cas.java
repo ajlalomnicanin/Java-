1e055a31878c96719cb045701a4cf3a00a1fc461
ajlalomnicanin <ajlalomnicanin11@gmail.com>
2024-02-27 17:42:52 +0100

    Java programiranje

---
+++ Drugi_cas.java
@@ -0,0 +1,19 @@
+public class Drugi_cas {
+//    psvm + enter
+//    main + enter
+public static void main(String[] args) {
+    System.out.print("Ana "); // samo stampanje bez prelaska u sledeci red
+    System.out.println("Markovic"); //
+    System.out.println(); //sledeci red
+    System.out.println("Jana"); //stampanje sa prelaskom u sledeci red
+    System.out.println("Joviic");
+
+    System.out.println(568);
+//    ; za program predstavlja jednu liniju ili deo koda
+//    komentari
+//    ako zelimo da iskomentarisemo jednu liniju koda onda stavimo //
+//    ovo je jednolinijski komentar
+//    ukoliko zeli o da iskomntarisemo vise linija koda onda stvimo /* */
+
+}
+}
--- Prvi_cas.java
+++ Prvi_cas.java
@@ -1,5 +1,17 @@
 public class Prvi_cas {
     public static void main(String[] args) {
         System.out.println("Prvi cas");
+//        Uvod u Java programiranje
+//        Nazivi projekata nam pocinju velikim slovom
+//        Nazivi klasa nam pocinju velikim slovom
+//        Na pocetku programa pravimo main metoda
+//        1. nacin main + enter
+//        2.nacin psvm + enter
+//
+//        ispis na ekranu
+//        sout + enter
+        System.out.println("Ovde prosledjujemo poruku");
+        System.out.println("Java programiranje");
+//        ln oznacava predji na sledeci red
     }
 }
